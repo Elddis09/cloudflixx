4d15c90f0ccdac8fd9bcd3d879abb6b9fb8522e6
Elddis09 <adis.anjeli01@gmail.com>
2024-04-05 10:29:32 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,22 @@
+<!-- Footer Component -->
+<div class="w-full bg-develobe-900 h-[320px] text-white flex flex-row pt-12">
+    <div class="w-6/12 pl-28 flex flex-col">
+        <span class="italic font-quicksand text-4xl font-bold">CloudFlixx</span>
+        <span class="font-poppins text-lg mt-4">Jelajahi Dunia Di Atas Awan <br>Temukan Inspirasi Tontonanmu 🚀 <br>#ExploreCloudscape </span>
+        <span class="font-poppins text-lg">🌸R.Angel</span>
+        
+        <span class="font-poppins text-lg mt-4">&copy; 2024 CloudFlixx</span>
+    </div>
+    <div class="w-3/12 flex flex-col">
+        <span class="font-poppins font-bold text-lg">Website</span>
+        <a href="/" class="font-poppins text-lg mt-4 hover:text-develobe-500 duration-200">Home</a>
+        <a href="/movies" class="font-poppins text-lg mt-4 hover:text-develobe-500 duration-200">Movies</a>
+        <a href="/tv-shows" class="font-poppins text-lg mt-4 hover:text-develobe-500 duration-200">TV Shows</a>
+    </div>
+    <div class="w-3/12 flex flex-col">
+        <span class="font-poppins font-bold text-lg">Social</span>
+        <a href="https://www.instagram.com/ddis_re09/" target="_blank" class="font-poppins text-lg mt-4 hover:text-develobe-500 duration-200">Instagram</a>
+        <a href="https://github.com/Elddis09" target="_blank" class="font-poppins text-lg mt-4 hover:text-develobe-500 duration-200">Github</a>
+    
+    </div>
+</div>
